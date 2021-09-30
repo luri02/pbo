2fd6f84211bb87cfe58e36d014d48c551eb2d6da
luri02 <luryapriyani@gmail.com>
2021-09-30 23:24:08 +0700

    pbo2

--- pert2/pbotugas2.cpp
+++ pert2/pbotugas2.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-using namespace std;
+#include<iostream>   //header yang dibutuhkan untuk input dan output
+using namespace std; 
 
-int main(){
-    int umur, thn;
-    cout<<"masukkan tahun lahir = ";
-    cin>> thn;
-    umur = 2021-thn;
-    cout<<"umur anda = "<<umur;
+int main(){  //statement yang digunakan saat program dimulai
+    int umur, thn;  //mendeklarasikan var umur dan tahun
+    cout<<"masukkan tahun lahir = "; //untuk mencetak statement dalam tanda kutip (perintah memasukan tahun lahir)
+    cin>> thn;  //untuk input dari user saat digunakan
+    umur = 2021-thn;  //rumus (umur sekarang dikurang tahun lahir)
+    cout<<"umur anda = "<<umur;  //untuk mencetak statement dalam tanda kutip (menampilkan umur)
     
 
-    return 0;
+    return 0; // perintah untuk mengakhiri program 
 }
