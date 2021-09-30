5fa5e356810102b080a6e7a0c1c3d044781804c1
luri02 <luryapriyani@gmail.com>
2021-09-30 22:41:33 +0700

    tes

---
+++ pert2/pbotugas2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int umur, thn;
+    cout<<"masukkan tahun lahir = ";
+    cin>> thn;
+    umur = 2021-thn;
+    cout<<"umur anda = "<<umur;
+    
+
+    return 0;
+}
