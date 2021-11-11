cf9838c2dc882b346b76cb231d759b1d3fe4f58d
luri02 <luryapriyani@gmail.com>
2021-11-11 12:50:59 +0700

    uts

---
+++ soal5/no5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+	int n,j,i,s;
+	
+	cout<<"Masukan Jumlah baris = ";cin>>n;
+	
+	for(i = 1;i<=n;i++){
+		for( s = 1;s<n;s++)
+		cout<<" ";
+		for( j = 1;j<= i;j++)
+		cout<<"*";
+		cout<<"\n";
+	}
+	return 0;
+}
